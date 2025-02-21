55a4a3ec1d35fcd1c784311c441e591f6b988ff0
Ryan Stam <ryans314@mit.edu>
2025-02-21 16:20:35 -0500

    completed studio assignment, added some styling

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
 import { createApp, Vue } from "https://mavue.mavo.io/mavue.js";
 
-let app = createApp({
-	data: {
-		tasks: [
-			{
-				done: false,
-				title: "",
-				active: true,
-			}
-		]
-	},
+let app = createApp(
+  {
+    data: {
+      tasks: [
+        {
+          done: false,
+          title: "",
+          active: true,
+        },
+      ],
+    },
 
-	methods: {
-		addItem() {
-			this.tasks.push({});
+    methods: {
+      addItem(i = this.tasks.length) {
+        this.tasks.splice(i + 1, 0, {});
 
-			this.setActive();
-		},
+        this.setActive();
+      },
 
-		deleteItem(i) {
-			this.tasks.splice(i, 1);
-			this.setActive(i - 1);
-		},
+      deleteItem(i) {
+        this.tasks.splice(i, 1);
+        this.setActive(i - 1);
+      },
 
-		deleteItemIfEmpty(i) {
-			if (!this.tasks[i]?.title) {
-				this.deleteItem(i);
-			}
-		},
+      deleteItemIfEmpty(i) {
+        if (!this.tasks[i]?.title) {
+          this.deleteItem(i);
+          //   this.setActive(i - 1);
+        }
+      },
 
-		clearCompleted () {
-			this.tasks = this.tasks.filter(task => !task.done);
-		},
+      clearCompleted() {
+        this.tasks = this.tasks.filter((task) => !task.done);
+      },
 
-		setActive(i) {
-			for (let task of this.tasks) {
-				task.active = false;
-			}
-
-			if (i >= 0 && this.tasks[i]) {
-				this.tasks[i].active = true;
-			}
-			else if (this.tasks.length > 0) {
-				this.tasks.at(-1).active = true;
-			}
-		},
-	}
-}, "#app");
+      setActive(i) {
+        for (let task of this.tasks) {
+          task.active = false;
+        }
+        // i %= this.tasks.length;
+        if (i >= 0 && this.tasks[i]) {
+          this.tasks[i].active = true;
+        } else if (this.tasks.length > 0) {
+          this.tasks.at(-1).active = true;
+        }
+      },
+    },
+  },
+  "#app"
+);
 
 // For debugging
 globalThis.app = app;
